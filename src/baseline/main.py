9c2b65d615c5690ac7de03aaed2046577f1986e1
maximilianmatscher <ge64sel>
2025-03-11 13:46:51 +0100

    Add recency model

--- src/baseline/main.py
+++ src/baseline/main.py
@@ -1,8 +1,5 @@
 import ast
-import numpy as np
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-from scipy.sparse import csr_matrix
 
 class PopularityModel:
     def __init__(self, df: pd.DataFrame):
@@ -24,8 +21,39 @@ class PopularityModel:
                                .reset_index(name="click_count")
                                .sort_values("click_count", ascending=False))
 
-    def predict(self, top_n=10):
-        """Predicts the top N items for a given user using collaborative filtering."""
-        top_items = self.popularityList.head(top_n).copy()
-        top_items["score"] = top_items["click_count"] / top_items["click_count"].max()
+    def predict(self, items=None, top_n=10):
+        """Predicts the top N items/supplied items for a given user using popularity scores."""
+        if items is None:
+            top_items = self.popularityList.head(top_n).copy()
+        else:
+            top_items = self.popularityList.copy()
+            top_items = top_items[top_items["news_id"].isin(items)]
+
+        top_items["score"] = top_items["click_count"] - top_items["click_count"].min()
+        top_items["score"] = top_items["score"] / top_items["score"].max()
         return top_items.set_index("news_id")["score"]
+
+
+class RecencyModel:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+        self.latest_timestamps = None
+
+    def fit(self):
+        """Tracks the first recent impression timestamp for each news article."""
+        # Ensure impressions column is correctly formatted
+        self.df["impressions"] = self.df["impressions"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
+
+        # Convert list of tuples into DataFrame format
+        interactions = self.df.explode("impressions")
+
+        # Extract news_id values
+        interactions = pd.DataFrame(interactions["impressions"].tolist(), columns=["news_id", "click"], index=interactions.index)
+        interactions["timestamp"] = self.df.loc[interactions.index, "timestamp"].values
+
+        # Compute latest timestamp for each news article
+        self.latest_timestamps = interactions.groupby("news_id")["timestamp"].min().reset_index()
+
+    def predict(self, top_n=10):
+        """Returns the most recent N articles."""
+        return self.latest_timestamps.sort_values("timestamp", ascending=False).head(top_n).set_index("news_id")
